9e4542d55f6fed8761bcad7b22c01ba99bf0d0f3
Stanislav Cmakal <stanislav.cmakal@gmail.com>
2023-09-20 10:43:44 +0100

    Changing 9k campaign to include campaign_id

--- src/examples/9k.js
+++ src/examples/9k.js
@@ -1,3 +1,6 @@
+var PROMO_ID_COOKIE_NAME = 'promo_id';
+var CAMPAIGN_ID_NAME = 'promo_id';
+var CAMPAIGN_ID = '16284950';
 var CAMPAIGN_START_DATE = '2023/09/20';
 
 function getValue(visitor, key) {
@@ -12,6 +15,29 @@ function getValue(visitor, key) {
   return def;
 }
 
+function getCookie(name) {
+  function escape(s) { 
+    return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, '\\$1'); 
+  }
+  var match = document.cookie.match(RegExp('(?:^|;\\s*)' + escape(name) + '=([^;]*)'));
+  return match ? match[1] : null;
+}
+
+function hasSeenCampaign() {
+  var promoCookie = getCookie(PROMO_ID_COOKIE_NAME);
+  if (promoCookie === null) {
+    var walletCookie = getCookie('wallet_utm');
+    if (walletCookie && typeof walletCookie === 'string') {
+      var params = new URLSearchParams(walletCookie);
+      var campaign = params.get(CAMPAIGN_ID_NAME) || null;
+      return !!(String(campaign) === CAMPAIGN_ID)
+    }
+    return false;
+  }
+  return !!(promoCookie && String(promoCookie) === CAMPAIGN_ID) 
+}
+
+
 function isNewUser(dateRegistered) {
   return !!(dateRegistered && dateRegistered >= new Date(CAMPAIGN_START_DATE))
 }
@@ -23,6 +49,7 @@ if (createdOn !== null) {
 }
 var _hasEnrolled = getValue(visitor, 'loyalty_enrolled').value || false;
 var _isNewUser = isNewUser(createdOn);
+var _hasSeenCampaign = hasSeenCampaign();
 
 // condition to show banner
-(!_hasEnrolled && _isNewUser)
+(!_hasEnrolled && _isNewUser && _hasSeenCampaign)
